dcb1776bbd2eff99338681f73921b7263f033a07
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2023-05-20 19:58:03 +0800

    Moved init_db.py functionality into scripts/init_db.sql file

---
+++ scripts/init_db.sql
@@ -0,0 +1,192 @@
+/*
+ * A1
+ */
+
+CREATE TABLE IF NOT EXISTS words_A1_nouns (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_A1_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_A1_phrasal_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_A1_adjectives (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+/*
+ * A2
+ */
+
+CREATE TABLE IF NOT EXISTS words_A2_nouns (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_A2_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_A2_phrasal_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_A2_adjectives (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+/*
+ * B1
+ */
+
+ CREATE TABLE IF NOT EXISTS words_B1_nouns (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_B1_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_B1_phrasal_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_B1_adjectives (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+/*
+ * B2
+ */
+
+  CREATE TABLE IF NOT EXISTS words_B2_nouns (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_B2_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_B2_phrasal_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_B2_adjectives (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+/*
+ * C1
+ */
+
+ CREATE TABLE IF NOT EXISTS words_C1_nouns (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_C1_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_C1_phrasal_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_C1_adjectives (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+/*
+ * C2
+ */
+
+ CREATE TABLE IF NOT EXISTS words_C2_nouns (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_C2_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_C2_phrasal_verbs (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+
+CREATE TABLE IF NOT EXISTS words_C2_adjectives (
+                    id serial PRIMARY KEY,
+                    word VARCHAR(64) UNIQUE NOT NULL,
+                    transcription VARCHAR(64),
+                    translation VARCHAR(64) NOT NULL,
+                    definition TEXT(4096));
+                    
