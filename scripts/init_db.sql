c8686ba9bfa4910ed51b7b5c355d83f3f0e4bc01
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2023-05-22 21:25:43 +0800

    Fix issue with definition row

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -7,28 +7,28 @@ CREATE TABLE IF NOT EXISTS words_A1_nouns (
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_A1_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_A1_phrasal_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_A1_adjectives (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 /*
  * A2
@@ -39,28 +39,28 @@ CREATE TABLE IF NOT EXISTS words_A2_nouns (
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_A2_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_A2_phrasal_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_A2_adjectives (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 /*
  * B1
@@ -71,28 +71,28 @@ CREATE TABLE IF NOT EXISTS words_A2_adjectives (
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_B1_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                   definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_B1_phrasal_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_B1_adjectives (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 /*
  * B2
@@ -103,28 +103,28 @@ CREATE TABLE IF NOT EXISTS words_B1_adjectives (
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_B2_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_B2_phrasal_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_B2_adjectives (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 /*
  * C1
@@ -135,28 +135,28 @@ CREATE TABLE IF NOT EXISTS words_B2_adjectives (
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_C1_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_C1_phrasal_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_C1_adjectives (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 /*
  * C2
@@ -167,26 +167,26 @@ CREATE TABLE IF NOT EXISTS words_C1_adjectives (
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_C2_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_C2_phrasal_verbs (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
 
 CREATE TABLE IF NOT EXISTS words_C2_adjectives (
                     id serial PRIMARY KEY,
                     word VARCHAR(64) UNIQUE NOT NULL,
                     transcription VARCHAR(64),
                     translation VARCHAR(64) NOT NULL,
-                    definition TEXT(4096));
+                    definition TEXT);
                     
