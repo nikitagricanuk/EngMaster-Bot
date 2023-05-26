5915a1262ce518eb0dd9ce562d348c23ee76b2cc
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2023-05-26 23:40:09 +0800

    Fixed floating error issue

--- handlers/exercise.py
+++ handlers/exercise.py
@@ -17,6 +17,8 @@ from words import words
 import random
 from random import randint as rint
 
+import logging
+
 # , DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT
 from config import WORD_TYPES
 from databases.db import connect_db
@@ -70,7 +72,8 @@ async def start_session(message: Message, state: FSMContext):
 @router.callback_query(Exercisig.choose_level, text='A1')
 async def start_exercise_A1(callback: CallbackQuery, state: FSMContext):
     await callback.message.delete()
-    await new_exercise(callback, state, 'A1')
+    logging.error('In start_exercise_A1() function')
+    await new_exercise(callback, state, 'a1')
 
 
 @router.callback_query(Exercisig.choose_level, text='A2')
@@ -116,13 +119,13 @@ async def stop_session(callback: CallbackQuery, state: FSMContext):
     else:
         await callback.message.answer(
             f'''
-                <b>Результаты</b>
+            <b>Результаты</b>
 
-                Всего слов: {words_num}
-                Всего правильных ответов: {rights}
-                Всего неправильных ответов: {words_num-rights}
+            Всего слов: {words_num}
+            Всего правильных ответов: {rights}
+            Всего неправильных ответов: {words_num-rights}
 
-                <b>{round((rights*100)/words_num, 1)}% ваших ответов являются правильными!</b>
+            <b>{round((rights*100)/words_num, 1)}% ваших ответов являются правильными!</b>
             ''',
             reply_markup=create_kb(start_kb_list))
 
@@ -154,30 +157,82 @@ async def false_answer(callback: CallbackQuery, state: FSMContext):
 async def new_exercise(callback, state, level):
     await state.set_state(Exercisig.exercise)
 
+    logging.error('In new_exercise() function 1!')
+
     kind = random.choice(WORD_TYPES)
     word = new_word(level, kind, rint(0, get_number_of_words(level, kind)))
 
     await state.update_data(words_num=0, rights=0)
     await state.update_data(word=word)
 
+    logging.error('In new_exercise() function 2!')
+
     await callback.message.answer('Сессия началась! Вы можете в любой момент закончить её нажатием кнопки на клавиатуре и узнать свои результаты', reply_markup=ReplyKeyboardRemove())
-    await callback.message.answer(f'<b>{word[0]} — ...</b>', reply_markup=create_exercise_kb(word).as_markup())
+    await callback.message.answer(f'<b>{word[0][0]} — ...</b>', reply_markup=create_exercise_kb(word).as_markup())
 
 
 def new_word(level, kind, id):
     db_connection = connect_db()
     cursor = db_connection.cursor()
 
+    logging.error('In new_word() function 1!')
+
     cursor.execute(f'SELECT * FROM words_{level}_{kind} WHERE id = {id}')
     data = cursor.fetchall()
 
-    print(data)
+    logging.error('In new_word() function 2!')
+
+    incorrect_words = []
+    for i in range(3):
+        incorrect_word_id = rint(0, get_number_of_words(level, kind))
+        while incorrect_word_id == id:
+            tmp = rint(0, get_number_of_words(level, kind))
+            print(tmp)
+            incorrect_word_id = tmp
+        
+        cursor.execute(f'SELECT * FROM words_{level}_{kind} WHERE id = {incorrect_word_id}')
+        incorrent_word_translate = cursor.fetchall()
+        print(f'In table words_{level}_{kind} data is', incorrent_word_translate)
+        incorrect_words.append(incorrent_word_translate[0][3])
+
+    # In this section we determine the word order.
+    # Here we're randomly choose in which part of return value
+    # will be true and in which false.
+    # Genius hack, I know :)
+    word_order = rint(0, 3)
+    if word_order == 0:
+        word1 = 'true'
+        word2 = 'false'
+        word3 = 'false'
+        word4 = 'false'
+    elif word_order == 1:
+        word1 = 'false'
+        word2 = 'true'
+        word3 = 'false'
+        word4 = 'false'
+    elif word_order == 2:
+        word1 = 'false'
+        word2 = 'false'
+        word3 = 'true'
+        word4 = 'false'
+    else:
+        word1 = 'false'
+        word2 = 'false'
+        word3 = 'false'
+        word4 = 'true'
+
+#          [('Word', 'Transcription'), ('Translation1', 'true'),
+#           ('Translation2', 'false'), ('Translation3', 'false'), 
+#           ('Translation4', 'false')]
+    return [(str(data[0][1]), str(data[0][2])), (str(data[0][3]), word1),
+            (str(incorrect_words[0]), word2), (str(incorrect_words[1]), word3),
+            (str(incorrect_words[2]), word4)] 
 
 
 def get_number_of_words(level, kind):
     db_connection = connect_db()
     cursor = db_connection.cursor()
-
+    logging.error('In get_number_of_words() function 1!')
     cursor.execute(f'SELECT * FROM words_{level}_{kind}')
-
+    logging.error('In get_number_of_words() function 2!')
     return cursor.rowcount
